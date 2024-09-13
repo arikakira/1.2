79230702137f3522b30772410101b98496ce0c0f
arikakira <samanthal242@nycstudents.net>
2024-09-13 15:39:11 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 2;
+        double dec = 1.4;
+        boolean state = true;
+        String words = " that";
+        double sum = num + dec;
+        System.out.println(sum);
+        System.out.println(state + words);
+    }
+ }
